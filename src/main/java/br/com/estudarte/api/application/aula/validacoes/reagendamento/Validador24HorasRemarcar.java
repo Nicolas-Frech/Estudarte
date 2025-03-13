6be911b4a0bfc768639c4b6b41716b3aeb3b393d
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-03-13 13:12:03 -0300

    Refactor validations

--- src/main/java/br/com/estudarte/api/application/aula/validacoes/reagendamento/Validador24HorasRemarcar.java
+++ src/main/java/br/com/estudarte/api/application/aula/validacoes/reagendamento/Validador24HorasRemarcar.java
@@ -7,7 +7,8 @@ import br.com.estudarte.api.infra.exception.ValidacaoException;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Component
 public class Validador24HorasRemarcar implements ValidadorReagendarAula {
@@ -21,9 +22,9 @@ public class Validador24HorasRemarcar implements ValidadorReagendarAula {
     @Override
     public void validar(AulaAtualizacaoDTO dto) {
         AulaEntity aula = aulaRepository.buscarPorId(dto.aulaId());
-        var horarioOriginal = aula.getData();
+        var agora = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
 
-        var diferencaEmHoras = Duration.between(horarioOriginal, dto.data()).toHours();
+        var diferencaEmHoras = Duration.between(agora, aula.getData()).toHours();
 
         if(diferencaEmHoras < 24) {
             throw new ValidacaoException("Somente reagendar aula com no mínimo 24 horas de antecedência!");
